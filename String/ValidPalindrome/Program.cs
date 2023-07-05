0aa1a2e23f75d9ba86afcc94eab26887c4941cfb
Edward Rush <edward.rush.developer@live.com>
2023-07-05 17:16:21 -0400

    Working on ValidPalindrome Code

--- String/ValidPalindrome/Program.cs
+++ String/ValidPalindrome/Program.cs
@@ -1,11 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+/*
 
+125. Valid Palindrome
+Difficulty: Easy
+Leet Code Problem
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+https://leetcode.com/problems/valid-palindrome/
+
+
+
+
+*/
 
 
 public class Palindrome
 {
 
+
+    public static bool IsPalindrome(string s) {
+        
+    }
 
 }
